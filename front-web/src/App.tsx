6bdcb14b7b26d015055a2b50e192e62b9ffd1bab
Diego Sena <diegosilva.sena7@gmail.com>
2021-03-06 17:21:45 -0300

    Footer ok

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
+import Footer from './Footer';
 import Home from './Home';
 import NavBar from './Navbar';
 
 function App() {
   return (
-    <div >
+    < >
      <NavBar />
      <Home />
-    </div>
+    </>
   );
 }
 
---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,22 @@
+import './styles.css';
+import {ReactComponent as Linkedin} from './linkedin.svg';
+import {ReactComponent as Instagram} from './instagram.svg';
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            Desenvolvido por <a href="https://diegossena.com.br/" target="_new">
+                Diego Sena
+            </a>
+            <div className="footer-icons"></div>
+            <a href="https://www.linkedin.com/in/diego-sena-03916b139/" target="_new">
+                <Linkedin/>
+            </a>
+            
+            <a href="https://www.instagram.com/diegosena_7/" target="_new">
+                <Instagram/>
+            </a>
+        </footer>
+    )
+}
+export default Footer;
