0dcd1018020d2d19c47243bade3bcdcc80051d0a
Diego Sena <diegosilva.sena7@gmail.com>
2021-03-13 23:57:35 -0300

    Go deploy

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,19 +1,17 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-const base_url = 'https://diegosena7pizzaria.herokuapp.com';
-// const API_URL = process.env.REACT_APP_API_URL;
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
-  return axios(`${base_url}/products`)
+  return axios(`${API_URL}/products`)
 }
 
 export const fetchLocalMapBox = (local: string) => 
     axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 
   export function saveOrder(payload : OrderPayload){
-      return axios.post(`${base_url}/orders`, payload);
+      return axios.post(`${API_URL}/orders`, payload);
       }
